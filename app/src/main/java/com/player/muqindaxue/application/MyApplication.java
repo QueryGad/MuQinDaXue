99b8eb1c87bb5b7b6c2380e6f32675304707469b
muqindaxue@126.com <muqindaxue@126.com>
2017-02-13 14:14:34 +0800

    6.社区条目布局

--- app/src/main/java/com/player/muqindaxue/application/MyApplication.java
+++ app/src/main/java/com/player/muqindaxue/application/MyApplication.java
@@ -1,7 +1,13 @@
 package com.player.muqindaxue.application;
 
 import android.app.Application;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.lzy.ninegrid.NineGridView;
+import com.player.muqindaxue.R;
 import com.yolanda.nohttp.NoHttp;
 
 /**
@@ -14,9 +20,28 @@ public class MyApplication extends Application{
     public void onCreate() {
         super.onCreate();
         initInternet();
+        initFrendCirle();
     }
 
     private void initInternet() {
         NoHttp.initialize(this);
+    }
+
+    private void initFrendCirle() {
+//        Glide.
+        NineGridView.setImageLoader(new NineGridView.ImageLoader() {
+            @Override
+            public void onDisplayImage(Context context, ImageView imageView, String url) {
+                Glide.with(context).load(url)
+                        .placeholder(R.drawable.ic_default_color)//
+                        .error(R.drawable.ic_default_color)//
+                        .into(imageView);
+            }
+
+            @Override
+            public Bitmap getCacheImage(String url) {
+                return null;
+            }
+        });
     }
 }
